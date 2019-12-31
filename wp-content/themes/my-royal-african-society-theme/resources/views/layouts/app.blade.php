77f068c079f5d38f7c61afbb9c63d875d8fd3d04
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2019-12-31 06:21:59 +0100

    fixes events

--- wp-content/themes/my-royal-african-society-theme/resources/views/layouts/app.blade.php
+++ wp-content/themes/my-royal-african-society-theme/resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <body @php body_class() @endphp>
     @php do_action('get_header') @endphp
     @include('partials.header')
-    <div class="wrap" role="document" style="padding:0;margin-top:-4px;<?php if(is_front_page()) echo 'z-index: -1;top: -98px;position: relative;'; else if(is_single()) echo 'margin-bottom:140px'; else echo 'margin-bottom:50px;'; ?>" >
+    <div class="wrap" role="document" style="padding:0;margin-top:-4px;<?php if(is_front_page()) echo 'z-index: -1;top: -98px;'; else if(is_single()) echo 'margin-bottom:140px'; else echo 'margin-bottom:50px;'; ?>" >
       <div class="content">
         <main class="main">
           @yield('content')
